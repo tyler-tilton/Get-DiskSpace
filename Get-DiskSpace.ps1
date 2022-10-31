6c5c2ca02083a367c0b1317cda6ef1327eed5373
tyler-tilton <tylerltilton@gmail.com>
2022-10-31 09:48:06 -0400

    Initial Commit

---
+++ Get-DiskSpace.ps1
@@ -0,0 +1,31 @@
+<#
+.Synopsis
+   Quickly get free and used disk space from one or more remote machines in a format that I like.
+.DESCRIPTION
+   Quickly get free and used disk space from one or more remote machines in a format that I like.
+.EXAMPLE
+   Get-DiskSpace -Computers SV1 -Disk D
+.EXAMPLE
+   Get-DiskSpace -Computers SV1,SV2 -Disk C
+.INPUTS
+   Target computer name in a string or computers in a comma seperated array and a string for the disk drive letter.
+.OUTPUTS
+   System object listing the computer(s) drive, and free/used space.
+#>
+
+function Get-DiskSpace {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Drive,
+        
+        [Parameter(Mandatory = $true)]
+        [array] $Computers
+    )
+    
+    Foreach ($target in $Computers) {
+        Invoke-Command -ComputerName $target -ArgumentList $Drive { 
+            param($Drive) 
+            Get-PSDrive -Name $Drive 
+        } | Select-Object PSComputerName, Description, @{Name = "Used"; Expression = { $_.Used / 1GB } }, @{Name = "Free"; Expression = { $_.Free / 1GB } }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# General Information
+## Project Name: 
+Get-DiskSpace
+## Purpose: 
+Quickly get free and used disk space from one or more remote machines in a format that I like.
+## Author: 
+Tyler Tilton
+## Published
+10/31/2022
+# Description
+The command takes in a remote machine name or names (cast to an arry) and a drive letter and outputs the following:
+```
+PSComputerName Description             Used             Free
+-------------- -----------             ----             ----
+PCI-PVSN001    Data        15259.4007720947 1124.41172790527
+```
+The Computers parameter and the Drive parameter must be specified.
+# Usage
+```
+Get-DiskSpace -Computers SV1 -Disk D
+
+Get-DiskSpace -Computers SV1,SV2 -Disk C
+```
